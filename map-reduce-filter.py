0f7e2c6a52d7d684d4975f0f14a14b77be89c3a6
jaybarmeda4152 <121751261+jaybarmeda4152@users.noreply.github.com>
2023-03-15 00:14:00 +0530

    reduce function

--- map-reduce-filter.py
+++ map-reduce-filter.py
@@ -14,9 +14,29 @@ ane a function mathi je value return karse ene ek list ma store kartu jase.
 
 ## FILTER function ###
 
-a = [1,2,3,4,5]
-def funCube(digit):
-    return digit ** 3
+# a = [1,2,3,4,5]
+# def funCube(digit):
+#     return digit > 3
 
-# print(a)
-b = list(filter(funCube,a))
+# b = list(filter(funCube,a))
+# print(b)
+
+## REDUCE function ###
+'''
+reduce funciton a only 2 j argument le ana thi vadhare na le.
+ama avu hoy ke jyare function call thay tyare list na pela 2 element ane anu je result ave ane first argument ma store kare.
+pa6i next time first argument ma result hoy ane second argument ma next element hoy list no.
+'''
+from functools import reduce
+# def funCube(key,digit):
+#     if key > digit:
+#         print('true=> ',key)
+#         return key + digit
+#     else:
+#         print('false=> ',key)
+#         return key * 2
+
+a = [1,2,3,4,5,6]
+b = reduce(lambda x,y: x+1 if x > y else x*2,a)
+# b = reduce(funCube ,a)
+print(b)
